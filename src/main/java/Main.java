c307f587a772932b5dec9b81398d8bb861f5481d
dmc-account <55603701+dmc-account@users.noreply.github.com>
2019-09-20 18:15:18 -0700

    prints

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args){
+        System.out.println("Elon Musk");
+        int score=1500;
+        System.out.println("this american businessman created the company that is now known as spacex?");
+        System.out.print(score);
         // Put code you want to run here
         // Then press the green arrow to the left to run it
     }
